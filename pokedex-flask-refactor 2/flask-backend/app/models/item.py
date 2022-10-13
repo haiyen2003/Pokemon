7e29435103506b6d0a6c92446c0f064bd6fa353b
hai yen <nhyen.93@gmail.com>
2022-10-13 10:51:58 -0700

    commit main - models folder

--- pokedex-flask-refactor 2/flask-backend/app/models/item.py
+++ pokedex-flask-refactor 2/flask-backend/app/models/item.py
@@ -1,15 +1,31 @@
-from flask_sqlalchemy import SQLAlchemy
-import datetime
-db = SQLAlchemy()
+from .db import db
+from datetime import datetime
+
+class item(db.Model):
+    __tablename__= "types"
+
+    id = db.Column(db.Integer, primary_key=True)
+    happiness = db.Column(db.Integer)
+    imageUrl = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+
+    pokemonId = db.Column(db.Integer, db.ForeignKey('pokemons.id'))
+
+    createdAt = db.Column(db.Datetime, nullable = False, default= datetime.now())
+    updatedAt = db.Column(db.Datetime, nullable = False, default= datetime.now())
+
+    pokemons= db.relationship('Pokemon', back_populates ='items')
 
 
-class Item(db.Model):
-    __tablename__ ='items'
-    id = db.Column(db.Integer, primary_key = True)
-    happiness = db.Column(db.Integer, nullable = False)
-    imageUrl = db.Column(db.String(255), nullable = False)
-    name = db.Column(db.String(255), nullable = False)
-    price = db.Column(db.Integer, nullable = False)
-    pokemonId = db.Column(db.Integer, nullable = False)
-    createdAt = db.Column(db.Date, default = datetime.now(), nullable = False)
-    updatedAt = db.Column(db.Date, default = datetime.now(), nullable = False)
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'happiness': self.happiness,
+            'imageUrl': self.imageUrl,
+            'name': self.name,
+            'price': self.price,
+            'pokemonId': self.pokemonId,
+            'createdAt': self.createdAt,
+            'updatedAt': self.updatedAt
+        }
